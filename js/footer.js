3960506e2fc1e0bf6a84315caf47793502fdc080
Eden Vidal <edenvidal@gmail.com>
2023-01-22 17:17:41 +0200

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -3,10 +3,10 @@ const TEMPLATE = `
 <a class="footer-logo" href="/">
     <img src="/images/under-footer.svg">
 </a>
-<div class="footer-content">
+<p>
     We are a creative team, designing digital brands & products through solid
     strategy and research. We admire simplicity, communication and focused process.
-</div>
+</p>
 <div class="footer-links">
     <a href="https://www.behance.net/weareunder" target="_blank">
         Behance
