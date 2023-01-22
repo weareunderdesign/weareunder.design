94d928635b1925ee248ff38ba89e1eac34f11f97
Eden Vidal <edenvidal@gmail.com>
2023-01-22 17:17:36 +0200

    Remove footer from main

--- js/main.js
+++ js/main.js
@@ -15,41 +15,6 @@ const lottieAnimations = [
 const randomIndex = getRandomInt(lottieAnimations.length);
 const randomAnimation = lottieAnimations[randomIndex].animation;
 
-function addFooter() {
-  const TEMPLATE = `
-<footer>
-<a class="footer-logo" href="/">
-    <img src="/images/under-footer.svg">
-</a>
-<div class="footer-content">
-    We are a creative team, designing digital brands & products through solid
-    strategy and research. We admire simplicity, communication and focused process.
-</div>
-<div class="footer-links">
-    <a href="https://www.behance.net/weareunder" target="_blank">
-        Behance
-    </a>
-    <a href="https://www.linkedin.com/company/underdesign/" target="_blank">
-        Linkedin
-    </a>
-    <a href="https://www.instagram.com/under.design/" target="_blank">
-        Instagram
-    </a>
-    <a href="mailto:hello@weareunder.design">
-        Email
-    </a>
-</div>
-</footer>
-`;
-
-  class UnderFooter extends HTMLElement {
-    constructor() {
-      super();
-      this.innerHTML = TEMPLATE;
-    }
-  }
-  customElements.define("under-footer", UnderFooter);
-}
 const ScrollLottie = async (obj) => {
   // let anim = lottie.loadAnimation({
   //   container: document.querySelector(obj.target),
