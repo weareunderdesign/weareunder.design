0a58e33294b860eaa8127f56b496dae7c1cfda1d
Eden Vidal <edenvidal@gmail.com>
2023-03-06 20:57:13 +0200

    Almost ready

--- js/footer.js
+++ js/footer.js
@@ -2,38 +2,33 @@ function addFooter() {
   const TEMPLATE = `
   <div class="direction-column padding-xl gap-xl">
     <a href="/">
-      <img src="/images/under.svg" style="width: 160px;">
+      <img src="/images/under-footer.svg" style="width: 102px;">
     </a>
     <h4 class="box">
-      We are a creative team, designing digital brands & products through solid
-      strategy and research. We admire simplicity, communication and focused process.
+      under creates high-quality,<br>
+      well-designed, creative brands <br>
+      and products for creative people.
     </h4>
     <div class="direction-row">
       <a href="https://www.behance.net/weareunder" target="_blank">
-        <h4>Behance</h4>
+        <h4>Press</h4>
       </a>
       <a href="https://www.linkedin.com/company/underdesign/" target="_blank">
-        <h4>Linkedin</h4>
+        <h4>Privacy</h4>
       </a>
       <a href="https://www.instagram.com/under.design/" target="_blank">
-        <h4>Instagram</h4>
-      </a>
-      <a href="mailto:hello@weareunder.design">
-        <h4>Email</h4>
+        <h4>Terms & Conditions</h4>
       </a>
     </div>
     <div class="direction-row">
       <a href="https://www.behance.net/weareunder" target="_blank">
-        <h4>Behance</h4>
+        <h4>Newsletter</h4>
       </a>
       <a href="https://www.linkedin.com/company/underdesign/" target="_blank">
-        <h4>Linkedin</h4>
-      </a>
-      <a href="https://www.instagram.com/under.design/" target="_blank">
         <h4>Instagram</h4>
       </a>
-      <a href="mailto:hello@weareunder.design">
-        <h4>Email</h4>
+      <a href="https://www.instagram.com/under.design/" target="_blank">
+        <h4>Contact</h4>
       </a>
     </div>
   </div>
