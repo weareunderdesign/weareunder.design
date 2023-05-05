12034918c0a53b4affafdec55b30d8b6dfc3f7ca
atulbhatt-system32 <atulbhatt98@gmail.com>
2023-05-05 15:30:32 +0530

    feat: home anim run infinitely on loop

--- js/main.js
+++ js/main.js
@@ -49,36 +49,41 @@ const ScrollLottie = async (obj) => {
         },
       });
 
-      let timeObj = { currentFrame: 0 };
-      let endString =
-        obj.speed === "slow"
-          ? "+=2000"
-          : obj.speed === "medium"
-          ? "+=1000"
-          : obj.speed === undefined
-          ? "+=1250"
-          : "+=500";
-      ScrollTrigger.create({
-        trigger: obj.target,
-        scrub: true,
-        pin: true,
-        start: "top top",
-        end: endString,
-        onUpdate: (self) => {
-          if (obj.duration) {
-            gsap.to(timeObj, {
-              duration: obj.duration,
-              currentFrame: Math.floor(self.progress * (anim.totalFrames - 1)),
-              onUpdate: () => {
-                anim.goToAndStop(timeObj.currentFrame, true);
-              },
-              ease: "expo",
-            });
-          } else {
-            anim.goToAndStop(self.progress * (anim.totalFrames - 1), true);
-          }
-        },
+      //run the animation in loop
+      anim.addEventListener("complete", () => {
+        anim.playSegments([0, anim.totalFrames], true);
       });
+
+      // let timeObj = { currentFrame: 0 };
+      // let endString =
+      //   obj.speed === "slow"
+      //     ? "+=2000"
+      //     : obj.speed === "medium"
+      //     ? "+=1000"
+      //     : obj.speed === undefined
+      //     ? "+=1250"
+      //     : "+=500";
+      // ScrollTrigger.create({
+      //   trigger: obj.target,
+      //   scrub: true,
+      //   pin: true,
+      //   start: "top top",
+      //   end: endString,
+      //   onUpdate: (self) => {
+      //     if (obj.duration) {
+      //       gsap.to(timeObj, {
+      //         duration: obj.duration,
+      //         currentFrame: Math.floor(self.progress * (anim.totalFrames - 1)),
+      //         onUpdate: () => {
+      //           anim.goToAndStop(timeObj.currentFrame, true);
+      //         },
+      //         ease: "expo",
+      //       });
+      //     } else {
+      //       anim.goToAndStop(self.progress * (anim.totalFrames - 1), true);
+      //     }
+      //   },
+      // });
     });
 };
 
