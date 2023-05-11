afda0dc71ab42463f6f1c7f8d66205cc2782a016
atulbhatt-system32 <atulbhatt98@gmail.com>
2023-05-11 12:06:57 +0530

    feat: work path made dynamic in header

--- js/header.js
+++ js/header.js
@@ -1,12 +1,13 @@
 function addHeader() {
   const imagePath = `${location.origin}/images/under-header-logo.svg`;
+  const workPath = `${location.origin}/#work-section`;
   const TEMPLATE = `
   <div class="flex" >
         <a class="logo padding-xl" href="/">
             <img src="${imagePath}" />
         </a>
         <div class="panel padding-xl gap-xs" style="color:white;position:fixed; right:0;z-index:999;">
-            <a class="header-link" href="#work-section"><h4>Work</h4></a>
+            <a class="header-link" href=${workPath}><h4>Work</h4></a>
             <a class="header-link" href="https://store.weareunder.design"><h4>Store</h4></a>
             <a class="header-link" href="mailto:hello@weareunder.design"><h4>Contact</h4></a>
         </div>
