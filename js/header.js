60800b81ec1f0d4f0d8c8bc4b62cc973f71425bc
atulbhatt-system32 <atulbhatt98@gmail.com>
2023-05-11 16:01:37 +0530

    feat: header logo and sidebar link styles

--- js/header.js
+++ js/header.js
@@ -1,13 +1,11 @@
 function addHeader() {
-  const imagePath = `${location.origin}/images/under-header-logo.svg`;
-  const workPath = `${location.origin}/#work-section`;
   const TEMPLATE = `
   <div class="flex" >
         <a class="logo padding-xl" href="/">
-            <img src="${imagePath}" />
+            <img src="https://weareunder.design/images/under-header-logo.svg" />
         </a>
         <div class="panel padding-xl gap-xs" style="color:white;position:fixed; right:0;z-index:999;  mix-blend-mode: difference;">
-            <a class="header-link" href=${workPath}><h4>Work</h4></a>
+            <a class="header-link" href="#"><h4>Work</h4></a>
             <a class="header-link" href="https://store.weareunder.design"><h4>Store</h4></a>
             <a class="header-link" href="mailto:hello@weareunder.design"><h4>Contact</h4></a>
         </div>
