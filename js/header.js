337b83ba20c3a191aec76de73db9ba512ddf3580
atulbhatt-system32 <atulbhatt98@gmail.com>
2023-05-17 14:14:09 +0530

    feat: responsive header logo

--- js/header.js
+++ js/header.js
@@ -1,17 +1,34 @@
 function addHeader() {
-    const TEMPLATE = `
-        <a class="logo padding-xl mix-diff" href="/" style="position: fixed; z-index: 99;">
-            <img src="https://weareunder.design/images/under-header-logo.svg" />
-        </a>
-  `;
+  const TEMPLATE = `
+      <a class="logo padding-xl mix-diff" href="/" style="position: fixed; z-index: 99;">
+        <img src="https://weareunder.design/images/under-header-logo.svg" class="desktop"/>
+        <img src="../images/under-header-logo-small.svg" class="mobile"/>
+      </a>
+    `;
 
-    class UnderHeader extends HTMLElement {
-        constructor() {
-            super();
-            this.innerHTML = TEMPLATE;
-        }
+  class UnderHeader extends HTMLElement {
+    constructor() {
+      super();
+      this.innerHTML = TEMPLATE;
     }
-    customElements.define("under-header", UnderHeader);
+  }
+  customElements.define("under-header", UnderHeader);
+
+  const mediaQuery = window.matchMedia("(max-width: 767px)");
+  mediaQuery.addEventListener("change", handleMediaQueryChange);
+  handleMediaQueryChange(mediaQuery);
+
+  function handleMediaQueryChange(mediaQuery) {
+    const mobileImg = document.querySelector("under-header img.mobile");
+    const desktopImg = document.querySelector("under-header img.desktop");
+    if (mediaQuery.matches) {
+      mobileImg.style.display = "block";
+      desktopImg.style.display = "none";
+    } else {
+      mobileImg.style.display = "none";
+      desktopImg.style.display = "block";
+    }
+  }
 }
 
 addHeader();
