5f0a47c76f2ae93bafeab6bd4d0fb9c2f59f9a2a
Stanislava Shestakova <stanislavasalishcheva@yandex.ru>
2024-06-02 22:53:37 +0300

    Added other small changes

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ function addFooter() {
   const TEMPLATE = `
   <div class="row padding-xl gap-xl">
     <a href="/">
-      <img src="../images/under-footer.svg" style="width: 92px;">
+      <img src="https://weareunder.design/images/under-footer.svg" style="width: 92px;">
     </a>
     <h4 class="box">
       under creates high-quality,<br>
@@ -10,6 +10,9 @@ function addFooter() {
       and products for creative people.
     </h4>
     <div class="column">
+    <a href="https://weareunder.design/articles/">
+        <h4>Articles</h4>
+      </a>
       <a href="https://store.weareunder.design/">
         <h4>Privacy</h4>
       </a>
