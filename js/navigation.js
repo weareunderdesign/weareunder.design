79aa230e7ee287888c91b3f8e2ee9af13d6909fc
Stanislava Shestakova <stanislavasalishcheva@yandex.ru>
2024-06-19 02:01:22 +0300

    Testing image preloading

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,18 @@
+function preloadImages() {
+  const imageUrls = [
+    "https://weareunder.design/work/zoog/0.png",
+  ];
+
+  imageUrls.forEach((url) => {
+    const img = new Image();
+    img.src = url;
+  });
+}
+
+window.addEventListener('load', preloadImages);
+
+
+
 function addSidebar() {
   const TEMPLATE = `
   <nav class="column padding-xl gap-xs" style="
@@ -103,258 +118,258 @@ function addSidebar() {
 </div>
     `;
 
-    class UnderNavigation extends HTMLElement {
-      constructor() {
-          super();
-          this.innerHTML = TEMPLATE;
-      }
+  class UnderNavigation extends HTMLElement {
+    constructor() {
+      super();
+      this.innerHTML = TEMPLATE;
+    }
   }
   customElements.define("under-navigation", UnderNavigation);
 
   var count = 0;
 
   const resizerInterval = setInterval(function () {
-      if (count < 50) {
-          window.dispatchEvent(new Event("resize"));
-          count++;
-      } else clearInterval(resizerInterval);
+    if (count < 50) {
+      window.dispatchEvent(new Event("resize"));
+      count++;
+    } else clearInterval(resizerInterval);
   }, 100);
 
   window.addEventListener("resize", () => {
-      setInterval(function () {
-          if (count < 50) {
-              window.dispatchEvent(new Event("resize"));
-              count++;
-          } else clearInterval(resizerInterval);
-      }, 100);
+    setInterval(function () {
+      if (count < 50) {
+        window.dispatchEvent(new Event("resize"));
+        count++;
+      } else clearInterval(resizerInterval);
+    }, 100);
   });
 
   const animations = $(".lottie-animation");
 
   function getRandomInt(max) {
-      return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
   }
 
   $(".sidebar").on("mouseover", function () {
-      // document.body.style.overflow = "hidden";
+    // document.body.style.overflow = "hidden";
   });
 
   $(".sidebar").on("mouseout", function () {
-      document.body.style.overflow = "auto";
+    document.body.style.overflow = "auto";
   });
 
   $("#nav-work").on("click", function () {
-      window.scrollTo({ top: 0 });
-      $(".hero-section").css("display", "none");
-      $("#under-nav").css("display", "none");
-      $("#works-wrapper").css("display", "block");
-      $("#body-content").css("display", "none");
+    window.scrollTo({ top: 0 });
+    $(".hero-section").css("display", "none");
+    $("#under-nav").css("display", "none");
+    $("#works-wrapper").css("display", "block");
+    $("#body-content").css("display", "none");
   });
 
   $("#sidebar-work").on("mouseleave", function () {
-      $(".hero-section").css("display", "block");
-      $("#under-nav").css("display", "block");
-      $("#works-wrapper").css("display", "none");
-      $("#body-content").css("display", "block");
+    $(".hero-section").css("display", "block");
+    $("#under-nav").css("display", "block");
+    $("#works-wrapper").css("display", "none");
+    $("#body-content").css("display", "block");
   });
 }
 async function SideBarFunctionality() {
-"use strict";
+  "use strict";
 
-const sidebar = document.getElementsByClassName("sidebar")[0];
-if (sidebar) {
-  sidebar.setAttribute("data-ix", "sidebar");
-}
-const rotateMenu = document.getElementsByClassName("work")[0];
-if (rotateMenu) {
-  rotateMenu.setAttribute("data-ix", "show-sidebar");
-}
+  const sidebar = document.getElementsByClassName("sidebar")[0];
+  if (sidebar) {
+    sidebar.setAttribute("data-ix", "sidebar");
+  }
+  const rotateMenu = document.getElementsByClassName("work")[0];
+  if (rotateMenu) {
+    rotateMenu.setAttribute("data-ix", "show-sidebar");
+  }
 
-var Main = function () {};
-Main.main = function () {
-  var initTimer = new haxe_Timer(50);
-  initTimer.run = function () {
-    if (
-      window.document.readyState == "interactive" ||
-      window.document.readyState == "complete"
-    ) {
-      Main.init();
-      initTimer.stop();
-    }
+  var Main = function () { };
+  Main.main = function () {
+    var initTimer = new haxe_Timer(50);
+    initTimer.run = function () {
+      if (
+        window.document.readyState == "interactive" ||
+        window.document.readyState == "complete"
+      ) {
+        Main.init();
+        initTimer.stop();
+      }
+    };
   };
-};
-Main.init = function () {
-  Main.heroElement = window.document.getElementById("brand-sprints-section");
-  Main.projectsElement =
-    window.document.getElementsByClassName("projects")[0];
-  Main.sidebarElement = window.document.getElementsByClassName("sidebar")[0];
-  if (!Main.projectsElement) return;
-  var img_len = Main.projectsElement.children.length;
+  Main.init = function () {
+    Main.heroElement = window.document.getElementById("brand-sprints-section");
+    Main.projectsElement =
+      window.document.getElementsByClassName("projects")[0];
+    Main.sidebarElement = window.document.getElementsByClassName("sidebar")[0];
+    if (!Main.projectsElement) return;
+    var img_len = Main.projectsElement.children.length;
 
-  var _g21 = 0;
-  while (_g21 < img_len) {
-    var child1 = Main.projectsElement.children.item(_g21++);
-    if (child1.tagName == "IMG") {
-      var img = child1;
-      var key9 = img.id;
-      var _this9 = Main.navLinks.images;
-      if (__map_reserved[key9] != null) {
-        _this9.setReserved(key9, img);
+    var _g21 = 0;
+    while (_g21 < img_len) {
+      var child1 = Main.projectsElement.children.item(_g21++);
+      if (child1.tagName == "IMG") {
+        var img = child1;
+        var key9 = img.id;
+        var _this9 = Main.navLinks.images;
+        if (__map_reserved[key9] != null) {
+          _this9.setReserved(key9, img);
+        } else {
+          _this9.h[key9] = img;
+        }
+      }
+    }
+    var nav_len = Main.sidebarElement.children.length;
+    var _g4 = 0;
+    while (_g4 < nav_len) {
+      var child2 = Main.sidebarElement.children.item(_g4++);
+      if (child2.tagName == "A") {
+        var anchor = child2;
+        var anID = [anchor.id.split("-")];
+        if (anID[0].length == 2) {
+          var _this10 = Main.navLinks.anchors;
+          var key10 = anID[0][0];
+          if (__map_reserved[key10] != null) {
+            _this10.setReserved(key10, anchor);
+          } else {
+            _this10.h[key10] = anchor;
+          }
+          var this1 = Main.navLinks.colors;
+          var value = "#" + anID[0][1].toUpperCase();
+          var _this11 = this1;
+          var key11 = anID[0][0];
+          if (__map_reserved[key11] != null) {
+            _this11.setReserved(key11, value);
+          } else {
+            _this11.h[key11] = value;
+          }
+          anchor.addEventListener(
+            "mouseover",
+            (function (anID1) {
+              return function () {
+                Main.previewProject(anID1[0][0]);
+              };
+            })(anID)
+          );
+        }
+      }
+    }
+
+    new haxe_Timer(24).run = function () {
+      if (Main.latestScrollY != window.scrollY) {
+        Main.latestScrollY = window.scrollY;
+      }
+      if (Main.sidebarElement.style.display == "none") {
+        if (Main.stopMotionMode == false) {
+          Main.stopMotionMode = true;
+        }
       } else {
-        _this9.h[key9] = img;
+        Main.stopMotionMode = false;
       }
-    }
-  }
-  var nav_len = Main.sidebarElement.children.length;
-  var _g4 = 0;
-  while (_g4 < nav_len) {
-    var child2 = Main.sidebarElement.children.item(_g4++);
-    if (child2.tagName == "A") {
-      var anchor = child2;
-      var anID = [anchor.id.split("-")];
-      if (anID[0].length == 2) {
-        var _this10 = Main.navLinks.anchors;
-        var key10 = anID[0][0];
-        if (__map_reserved[key10] != null) {
-          _this10.setReserved(key10, anchor);
-        } else {
-          _this10.h[key10] = anchor;
-        }
-        var this1 = Main.navLinks.colors;
-        var value = "#" + anID[0][1].toUpperCase();
-        var _this11 = this1;
-        var key11 = anID[0][0];
-        if (__map_reserved[key11] != null) {
-          _this11.setReserved(key11, value);
-        } else {
-          _this11.h[key11] = value;
-        }
-        anchor.addEventListener(
-          "mouseover",
-          (function (anID1) {
-            return function () {
-              Main.previewProject(anID1[0][0]);
-            };
-          })(anID)
-        );
-      }
-    }
-  }
+    };
+  };
 
-  new haxe_Timer(24).run = function () {
-    if (Main.latestScrollY != window.scrollY) {
-      Main.latestScrollY = window.scrollY;
+  Main.previewProject = function (id) {
+    var _this = Main.navLinks.colors;
+    if (
+      (__map_reserved[id] != null
+        ? _this.existsReserved(id)
+        : _this.h.hasOwnProperty(id)) == true
+    ) {
+      var _this1 = Main.navLinks.colors;
+      $(".hero-section svg").hide();
+      Main.sidebarElement.style.backgroundColor =
+        __map_reserved[id] != null ? _this1.getReserved(id) : _this1.h[id];
     }
-    if (Main.sidebarElement.style.display == "none") {
-      if (Main.stopMotionMode == false) {
-        Main.stopMotionMode = true;
-      }
-    } else {
-      Main.stopMotionMode = false;
+
+    var _this2 = Main.navLinks.images;
+    if (
+      (__map_reserved[id] != null
+        ? _this2.existsReserved(id)
+        : _this2.h.hasOwnProperty(id)) == true
+    ) {
+      var _this3 = Main.navLinks.images;
+      var newImage =
+        __map_reserved[id] != null ? _this3.getReserved(id) : _this3.h[id];
+
+      Main.heroElement.style.backgroundImage = "url(" + newImage.src + ")";
     }
   };
-};
 
-Main.previewProject = function (id) {
-  var _this = Main.navLinks.colors;
-  if (
-    (__map_reserved[id] != null
-      ? _this.existsReserved(id)
-      : _this.h.hasOwnProperty(id)) == true
-  ) {
-    var _this1 = Main.navLinks.colors;
-    $(".hero-section svg").hide();
-    Main.sidebarElement.style.backgroundColor =
-      __map_reserved[id] != null ? _this1.getReserved(id) : _this1.h[id];
-  }
-
-  var _this2 = Main.navLinks.images;
-  if (
-    (__map_reserved[id] != null
-      ? _this2.existsReserved(id)
-      : _this2.h.hasOwnProperty(id)) == true
-  ) {
-    var _this3 = Main.navLinks.images;
-    var newImage =
-      __map_reserved[id] != null ? _this3.getReserved(id) : _this3.h[id];
-
-    Main.heroElement.style.backgroundImage = "url(" + newImage.src + ")";
-  }
-};
-
-Main.setStopMotionFrame = function (position, sequence) {
-  if (sequence.images.length > 0) {
-    var framePos = Math.floor((sequence.images.length - 1) * position);
-    Main.heroElement.style.backgroundImage =
-      "url(" + sequence.images[framePos].src + ")";
-  }
-};
-var Std = function () {};
-Std.parseInt = function (x) {
-  var v = parseInt(
-    x,
-    x && x[0] == "0" && (x[1] == "x" || x[1] == "X") ? 16 : 10
-  );
-  if (isNaN(v)) {
-    return null;
-  }
-  return v;
-};
-var haxe_IMap = function () {};
-var haxe_Timer = function (time_ms) {
-  var me = this;
-  this.id = setInterval(function () {
-    me?.run();
-  }, time_ms);
-};
-haxe_Timer.prototype = {
-  stop: function () {
-    if (this.id == null) {
-      return;
+  Main.setStopMotionFrame = function (position, sequence) {
+    if (sequence.images.length > 0) {
+      var framePos = Math.floor((sequence.images.length - 1) * position);
+      Main.heroElement.style.backgroundImage =
+        "url(" + sequence.images[framePos].src + ")";
     }
-    clearInterval(this.id);
-    this.id = null;
-  },
-  run: function () {},
-};
-var haxe_ds_StringMap = function () {
-  this.h = {};
-};
-haxe_ds_StringMap.__interfaces__ = [haxe_IMap];
-haxe_ds_StringMap.prototype = {
-  setReserved: function (key, value) {
-    if (this.rh == null) {
-      this.rh = {};
-    }
-    this.rh["$" + key] = value;
-  },
-  getReserved: function (key) {
-    if (this.rh == null) {
+  };
+  var Std = function () { };
+  Std.parseInt = function (x) {
+    var v = parseInt(
+      x,
+      x && x[0] == "0" && (x[1] == "x" || x[1] == "X") ? 16 : 10
+    );
+    if (isNaN(v)) {
       return null;
-    } else {
-      return this.rh["$" + key];
     }
-  },
-  existsReserved: function (key) {
-    if (this.rh == null) {
-      return false;
-    }
-    return this.rh.hasOwnProperty("$" + key);
-  },
-};
+    return v;
+  };
+  var haxe_IMap = function () { };
+  var haxe_Timer = function (time_ms) {
+    var me = this;
+    this.id = setInterval(function () {
+      me?.run();
+    }, time_ms);
+  };
+  haxe_Timer.prototype = {
+    stop: function () {
+      if (this.id == null) {
+        return;
+      }
+      clearInterval(this.id);
+      this.id = null;
+    },
+    run: function () { },
+  };
+  var haxe_ds_StringMap = function () {
+    this.h = {};
+  };
+  haxe_ds_StringMap.__interfaces__ = [haxe_IMap];
+  haxe_ds_StringMap.prototype = {
+    setReserved: function (key, value) {
+      if (this.rh == null) {
+        this.rh = {};
+      }
+      this.rh["$" + key] = value;
+    },
+    getReserved: function (key) {
+      if (this.rh == null) {
+        return null;
+      } else {
+        return this.rh["$" + key];
+      }
+    },
+    existsReserved: function (key) {
+      if (this.rh == null) {
+        return false;
+      }
+      return this.rh.hasOwnProperty("$" + key);
+    },
+  };
 
-var __map_reserved = {};
-Main.latestScrollY = 0;
-Main.relatedScroll = 0;
-Main.stopMotionMode = true;
-Main.animations = [];
-Main.animationMap = new haxe_ds_StringMap();
-Main.navLinks = {
-  images: new haxe_ds_StringMap(),
-  anchors: new haxe_ds_StringMap(),
-  colors: new haxe_ds_StringMap(),
-};
-Main.main();
+  var __map_reserved = {};
+  Main.latestScrollY = 0;
+  Main.relatedScroll = 0;
+  Main.stopMotionMode = true;
+  Main.animations = [];
+  Main.animationMap = new haxe_ds_StringMap();
+  Main.navLinks = {
+    images: new haxe_ds_StringMap(),
+    anchors: new haxe_ds_StringMap(),
+    colors: new haxe_ds_StringMap(),
+  };
+  Main.main();
 }
 SideBarFunctionality();
 
