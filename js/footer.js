5ab0155dfab3811fefe3959b442dec893d9c8951
atulbhatt-system32 <atulbhatt98@gmail.com>
2025-01-21 15:08:47 +0530

    added newsletter redirect to shopify

--- js/footer.js
+++ js/footer.js
@@ -21,35 +21,49 @@ const footerTemplate = `
         well-designed, creative brands <br>
         and products for creative people.
       </h5>
+
       <div class="column gap-xs">
-        <a href="https://github.com/weareunderdesign" target="_blank">
-          <h5>github</h5>
-        </a>
-        <a href="mailto:hello@weareunder.design">
-          <h5>contact</h5>
-        </a>
-        <a href="https://weareunder.design/legal">
-          <h5>legal</h5>
-        </a>
-        <a href="https://store.weareunder.design/pages/newsletter" target="_blank">
-          <h5>newsletter</h5>
-        </a>
+        <div class="row gap-xl">
+          <div class="column gap-xs">
+            <a href="https://github.com/weareunderdesign" target="_blank">
+              <h5>github</h5>
+            </a>
+            <a href="mailto:hello@weareunder.design">
+              <h5>contact</h5>
+            </a>
+            <a href="https://weareunder.design/legal">
+              <h5>legal</h5>
+            </a>
+
+          </div>
+        
+          <div class="column align-start gap-xs">
+            <a href="https://www.youtube.com/@weareunderdesign">
+              <h5>youtube</h5>
+            </a>
+            <a href="https://www.instagram.com/under.design/" target="_blank">
+              <h5>instagram</h5>
+            </a>
+            <a href="https://x.com/underdesign_" target="_blank">
+              <h5>twitter</h5>
+            </a>
+            <a href="#" id="theme-toggle">
+              <h5 id="theme-name" class="opacity-l">system</h5>
+            </a>
+          </div>
+        </div>
+         <h5 id="subscribeButton">newsletter</h5>
+          <div class="hidden row" id="subscribeForm">
+            <h5>
+              <input type="email" id="emailInput" placeholder="Subscribe" style="outline:none; border:none; max-width:200px;">
+            </h5>
+            <div class="align-center hidden" id="submit-newsletter">
+              <svg-icon src="https://raincons.rnbw.dev/icons/arrow-right.svg" class="icon-s" ></svg-icon>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="column align-start gap-xs">
-        <a href="https://www.youtube.com/@weareunderdesign">
-          <h5>youtube</h5>
-        </a>
-        <a href="https://www.instagram.com/under.design/" target="_blank">
-          <h5>instagram</h5>
-        </a>
-        <a href="https://x.com/underdesign_" target="_blank">
-          <h5>twitter</h5>
-        </a>
-        <a href="#" id="theme-toggle">
-          <h5 id="theme-name" class="opacity-l">system</h5>
-        </a>
-      </div>
-    </div>
+    </div> 
   </div>
 </footer>
 `;
@@ -100,6 +114,7 @@ class underFooter extends HTMLElement {
     this.ensureSvgIconLoaded().then(() => {
       this.innerHTML = footerTemplate;
       this.initializeThemeToggle();
+      this.handleSubscribe();
     });
   }
 
@@ -132,6 +147,41 @@ class underFooter extends HTMLElement {
 
     updateThemeElementsVisibility();
   }
+  handleSubscribe() {
+
+    const subscribeButton = document.getElementById('subscribeButton');
+
+    if (!subscribeButton) return;
+    subscribeButton.addEventListener('click', (e) => {
+
+      const subscribeForm = document.getElementById('subscribeForm');
+      subscribeForm.classList.toggle('hidden');
+      //focus on input
+      const emailInput = document.getElementById('emailInput');
+      emailInput.focus();
+
+      //hide this button
+      e.target.style.display = 'none';
+
+      // Add event listener to email input for validation
+      emailInput.addEventListener('input', () => {
+        const submitButtonIcon = subscribeForm.querySelector('#submit-newsletter');
+        if (emailInput.validity.valid && emailInput.value) {
+          submitButtonIcon.classList.remove('hidden');
+        } else {
+          submitButtonIcon.classList.add('hidden');
+        }
+      });
+
+      const submitButton = document.getElementById('submit-newsletter');
+      submitButton.addEventListener('click', () => {
+        e.target.style.display = 'block';
+        subscribeForm.classList.add('hidden');
+        e.target.innerHTML = 'Thanks for subscribing!';
+        window.open('https://store.weareunder.design/pages/newsletter?email=' + emailInput.value);
+      });
+    });
+  }
 }
 
 customElements.define("under-footer", underFooter);
@@ -139,7 +189,11 @@ customElements.define("under-footer", underFooter);
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", handleSystemThemeChange);
 
 const currentYear = new Date().getFullYear();
-document.getElementById("year").innerHTML += currentYear;
+try {
+  document.getElementById("year").innerHTML += currentYear;
+} catch (error) {
+  console.error('Error in setting year:', error);
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   const themeName = document.querySelector("#theme-name");
@@ -204,3 +258,4 @@ function updateThemeImageNew(theme) {
     }
   }
 }
+
